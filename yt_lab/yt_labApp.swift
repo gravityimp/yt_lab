bec8c102eb8ae07b5035199378b0c3ab66ce4ac6
Yurii Tyshchenko <orty222@gmail.com>
2023-10-03 16:30:59 +0200

    Initial Commit

---
+++ yt_lab/yt_labApp.swift
@@ -0,0 +1,17 @@
+//
+//  yt_labApp.swift
+//  yt_lab
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct yt_labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
